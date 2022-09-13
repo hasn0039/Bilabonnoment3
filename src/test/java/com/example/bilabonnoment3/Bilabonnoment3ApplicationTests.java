f1f6f391f0481f76118c82970d9bcf4a277a0ace
hasnainahmad <hasn0039@stud.kea.dk>
2022-09-13 19:34:33 +0200

    Initial commit

---
+++ src/test/java/com/example/bilabonnoment3/Bilabonnoment3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bilabonnoment3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bilabonnoment3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
